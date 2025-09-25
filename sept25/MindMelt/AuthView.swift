5e44800e0a5efe2c797010ca54fc2a78a0767697
MaisonMunchnix <nicole320enriquez@gmail.com>
2025-09-25 09:56:11 +0800

    added logout and notif

---
+++ sept25/MindMelt/AuthView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthView.swift
+//  MindMelt
+//
+//  Created by Kyla Enriquez on 9/22/25.
+//
+
+//import Foundation
+//
+//struct AuthView : View {
+//    
+//    @StateObject private var supabase = SupabaseManager.shared
+//    @State private var email: String = ""
+//    @State private var password: String = ""
+//    @State private var isSignup: Bool = false
+//    
+//    var
+//}
---
+++ sept25/MindMelt/Reminder.swift
@@ -0,0 +1,105 @@
+//
+//  Reminder.swift
+//  MindMelt
+//
+//  Created by Kyla Enriquez on 9/22/25.
+//
+
+import Foundation
+
+import SwiftUI
+ 
+struct Reminder : View{
+    @State var fakebutton = false
+    var body: some View{
+        ZStack{
+            Color.black.ignoresSafeArea()
+            
+            VStack{
+                HStack{
+                    HStack{
+                        Image("mm")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width:20, height: 30)
+                        
+                        Text("Set Reminder")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                    }
+                    .padding()
+                    
+                    Spacer()
+                    
+                    Text("Cancel").foregroundColor(.red).padding()
+                }
+                
+//                Divider()
+//                    .frame(height: 1)
+//                    .foregroundColor(.white)
+                
+                VStack{
+                    Image("thumb")
+                        .scaledToFit()
+                        .padding()
+                        .frame(width: 200, height: 150)
+                        .cornerRadius(20)
+                    
+                    Text("Google Firebase Studio in 23 minutes")
+                        .foregroundColor(.white)
+                        .fontWeight(.medium)
+                    
+                    
+                }
+                
+                VStack{
+                    HStack{
+                        Text("Set Date")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                        Spacer()
+                        Image(systemName: "calendar")
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    
+                    HStack{
+                        Text("Set Time")
+                            .foregroundColor(.white)
+                            .fontWeight(.semibold)
+                        Spacer()
+                        Image(systemName: "clock")
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                }
+                .padding()
+                .cornerRadius(50)
+                
+                Spacer()
+                Button(action:
+                        {
+                            fakebutton = true
+                        }
+                ){
+                    Text("Save reminder")
+                        .foregroundColor(.white)
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                        .padding()
+                        .background(Color.red)
+                        .cornerRadius(13)
+                }
+                
+            }
+            
+            
+        }
+       
+    }
+}
+ 
+ 
+#Preview {
+    Reminder()
+}
